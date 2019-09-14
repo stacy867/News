d53d513119f7828b45c092439c6364daf4f80a0a
stacy867 <stacymurenzi@gmail.com>
2019-09-14 13:49:51 +0200

    getting sourcerequest

--- app/views.py
+++ app/views.py
@@ -11,10 +11,10 @@ def index():
     View root page function that returns the index page and its data
     '''
     #getting sources
-    news_sources = get_source('news')
-    # print(news_sources)
+    news_sources = get_source('sports')
+    print(news_sources)
     title = 'Home -welcome to the newswebsite'
-    return render_template('index.html', title = title, news = news_sources)
+    return render_template('index.html', title = title,news = news_sources)
 
 
 # @app.route('/sources/<int:sources_id>')
@@ -26,10 +26,13 @@ def index():
 #     return render_template('source.html',id = sources_id)
 
 
-# @app.route('/everything/<int:articles_id>')
-# def source(articles_id):
+@app.route('/everything/<source_id>')
+def source(source_id):
 
-#     '''
-#     View source page function that returns the articles  page
-#     '''
-#     return render_template('articles.html',id = articles_id)
+    '''
+    View source page function that returns the articles  page
+    '''
+    news_articles = get_article('source_id')
+    # print(news_articles)
+    title = 'Home -welcome to articles'
+    return render_template('articles.html',id = sources_id, articles= news_articles)
